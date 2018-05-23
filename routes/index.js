014d99e93a7e39834b41b8ae5c9527be39449030
Sahiti <stadepalli@live.com>
2018-05-23 00:15:19 -0700

    storeData views

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/storeData', function(req, res) {
+router.post('/storeData', function(req, res, next) {
     var value_name = req.body.order;
-    res.send("Order successfully received" + value_name);
-    res.render('storeData', {'order' : value_name} );
+    // res.send("Order successfully received" + value_name);
+    // res.render('storeData', {'order' : value_name} );
+    const server = http.createServer((req,res)=>{
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('X-Foo', 'bar');
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('');
+      }).listen(port,()=>{
+        console.log('your app is started');
+    });
 });
 
 router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
