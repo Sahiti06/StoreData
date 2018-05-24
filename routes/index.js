75c7b6e97ed12326f7558fc523cc4c2493d684c4
Sahiti <stadepalli@live.com>
2018-05-23 19:12:14 -0700

    Views

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,15 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/storeData', function(req, res) {
+router.post('/storeData, function(req, res, next) {
+var value_name = req.body.order;
+res.send("Order succesfully received: " + value_name);
+});
+
+router.get('/storeData', function(req, res) {
     var finalOrder = req.body.order;
     res.render('storeData', { title: 'Results from ORDERS Collection'});
-    res.render("Success");
+    res.render(finalOrder);
 });
 
 router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
